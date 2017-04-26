d59a3bdd49443db8cd6283f23410659f65c9436c
Roberto Grave <robertoalexgv@outlook.com>
2017-04-26 17:05:41 -0600

    Inicio del proecto, estableciendo archivos y carpets necesarias.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog Prueba</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Blog</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<div class="blog-post">
+					<h2>Sample Title</h2>
+					<p>Jan 1, 2020 by <a href="#">Alex</a></p>
+					<div class="blog-post-image">
+						<img src="images/slide3.png">
+					</div>
+					<div class="blog-post-content">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae aut, nulla magnam fuga porro assumenda consequuntur nostrum ipsam. Fugit nam non eius quae. At facilis dolore quo magni voluptatum, itaque!
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+			fsdfd</div>
+		</div>
+		<div class="blo-post-footer">
+			<footer>
+				copiright
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
